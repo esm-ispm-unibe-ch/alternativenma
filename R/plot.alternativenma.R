899f875eaabd7a6f94502181ab2f465af4bb6834
Thodoris Papakonstantinou <mail@tpapak.com>
2019-01-10 14:12:47 +0100

    added plot functions arguments

--- R/plot.alternativenma.R
+++ R/plot.alternativenma.R
@@ -5,8 +5,13 @@
 # altnmaobject: An object of class alternativenma
 # eform: A logical indicating whether the exponential of effects should be drawn 
 # ordered: A logical indicating whether the effects should be drawn ordered  
+# xmin: minimum limit of x axis
+# xmax: maximum limit of x axis
+# treatmentplacement: The x value of treatment labels
+# effectplacement: The x value of the effect sizes
 
-plot.alternativenma<-function(altnmaobject, eform=TRUE, ordered=TRUE){
+
+plot.alternativenma<-function(altnmaobject, eform=TRUE, ordered=TRUE, xmin=0.2, xmax=2.8, treatmentplacement=0.3, effectplacement=2.0){
 
   library(ggplot2)
   library(grid)
@@ -45,19 +50,22 @@ plot.alternativenma<-function(altnmaobject, eform=TRUE, ordered=TRUE){
     p=ggplot(Effects)+
       geom_point(aes(Effects$NetwTE,Effects$steps),na.rm = T)
     p=p+geom_line(data=ForReapPlot,aes(NetwCI,StepsForPlot,group=StepsForPlot),na.rm = T)
-    p=p+geom_vline(xintercept = 1)
-
+    if (eform==TRUE){
+      p=p+geom_vline(xintercept = 1)
+    }else{
+      p=p+geom_vline(xintercept = 0)
+    }
     p=p + theme(axis.title.y=element_blank(),
                 axis.text.y=element_blank(),
                 axis.ticks.y=element_blank())
-    
-    p=p + annotate("text", x = rep(0.6, length(rownames(averages1))),
+    p=p + labs(x = "Effect vs fictional treatment of average performance")
+    p=p + annotate("text", x = rep(treatmentplacement, length(rownames(averages1))),
                  y = steps, label = c(rownames(averages1)))
     
-    p=p + annotate("text", x = rep(1.6, length(rownames(averages1))),
+    p=p + annotate("text", x = rep(effectplacement, length(rownames(averages1))),
                  y = steps, label = paste(round(NetwTE,2),"(",round(NetworkLCI,2),",",round(NetworkUCI,2),")"))
     
-    p
+    p + expand_limits(x=c(xmin,xmax))
 }
 
 
